0c96f5bbc5155f0d89095a50fbf821d324affd57
wangwei560 <Administrator@DESKTOP-O0COGNR>
2017-01-03 18:20:29 +0800

    设计部客户洽谈页面搭建

---
+++ src/main/java/com/zoj/bp/design/controller/ClientCtrl.java
@@ -0,0 +1,385 @@
+/**
+ * 
+ */
+package com.zoj.bp.design.controller;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zoj.bp.common.excption.BusinessException;
+import com.zoj.bp.common.excption.ReturnCode;
+import com.zoj.bp.common.model.Client;
+import com.zoj.bp.common.model.CommissionCost;
+import com.zoj.bp.common.model.InfoCost;
+import com.zoj.bp.common.model.Order;
+import com.zoj.bp.common.model.OrderApprove;
+import com.zoj.bp.common.model.OrderVisit;
+import com.zoj.bp.common.model.User;
+import com.zoj.bp.common.util.ResponseUtils;
+import com.zoj.bp.common.vo.DatagridVo;
+import com.zoj.bp.common.vo.Pagination;
+import com.zoj.bp.costmgr.infocostmgr.service.IInfoCostMgrService;
+import com.zoj.bp.marketing.service.IClientService;
+import com.zoj.bp.marketing.service.ICommissionCostService;
+import com.zoj.bp.marketing.service.IInfoCostService;
+import com.zoj.bp.marketing.service.IInfoerService;
+import com.zoj.bp.marketing.service.IOrderService;
+import com.zoj.bp.marketing.service.IOrderVisitService;
+import com.zoj.bp.sysmgr.usermgr.service.IUserService;
+
+/**
+ * @author wangw
+ *
+ */
+@Controller
+@RequestMapping("/marketing/clientMgr")
+public class ClientCtrl
+{
+	@Autowired
+	private IOrderService orderSvc;
+	
+	@Autowired
+	private IOrderVisitService orderVisitSvc;
+	
+	@Autowired
+	private IClientService clientSvc;
+	
+	@Autowired
+	private IInfoerService infoerSvc;
+	
+	@Autowired
+	private IInfoCostService infoCostSvc;
+	
+	@Autowired
+	private IInfoCostMgrService infoCostMgrSvc;
+	
+	@Autowired
+	private ICommissionCostService commissionCostSvc;
+	
+	@Autowired
+	private IUserService userSvc;
+	
+	@RequestMapping(value = "/toClientTraceView")
+	public String toClientTraceView() throws BusinessException
+	{
+		return "/marketing/clientTrace/index";
+	}
+	
+	
+	@RequestMapping(value = "/toClientNegotiationView")
+	public String toClientNegotiationView() throws BusinessException
+	{
+		return "/marketing/clientNegotiation/index";
+	}
+	
+	/**
+	 * 获取客户跟踪记录
+	 * @param pagination
+	 * @param name
+	 * @param tel
+	 * @param infoerName
+	 * @param status
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(value = "/getAllClientTrace")
+	@ResponseBody
+	public DatagridVo<Order> getAllClientTrace(Pagination pagination,@RequestParam(required=false) String name,
+			@RequestParam(required=false) String tel,@RequestParam(required=false) String infoerName,@RequestParam(required=false) String status, HttpSession session)
+	{
+		User loginUser = (User) session.getAttribute("loginUser");
+		String[] statusArr = null;
+		if (StringUtils.isNotEmpty(status)){
+			statusArr = status.split(",");
+			if(Arrays.asList(statusArr).contains("-1"))
+				statusArr = new String[]{"10","12","30","32","14"};
+		}else
+			statusArr = new String[]{"10","12","30","32","14"};
+		return orderSvc.getAllOrder(pagination,loginUser,name,tel,infoerName,statusArr);
+	}
+	
+	@RequestMapping(value = "/getAllClientCheck")
+	@ResponseBody
+	public DatagridVo<Order> getAllClientCheck(Pagination pagination,@RequestParam(required=false) String name,
+			@RequestParam(required=false) String tel,@RequestParam(required=false) String infoerName,@RequestParam(required=false) String status, HttpSession session)
+	{
+		User loginUser = (User) session.getAttribute("loginUser");
+		String[] statusArr = null;
+		if (StringUtils.isNotEmpty(status))
+			statusArr = status.split(",");
+		else
+			statusArr = new String[]{"30","62"};
+		return orderSvc.getAllOrder(pagination,loginUser,name,tel,infoerName,statusArr);
+	}
+	
+	/**
+	 * 获取客户洽谈记录
+	 * @param pagination
+	 * @param name
+	 * @param tel
+	 * @param infoerName
+	 * @param status
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(value = "/getAllClientNegotiation")
+	@ResponseBody
+	public DatagridVo<Order> getAllClientNegotiation(Pagination pagination,@RequestParam(required=false) String name,
+			@RequestParam(required=false) String tel,@RequestParam(required=false) String infoerName,@RequestParam(required=false) String status, HttpSession session)
+	{
+		User loginUser = (User) session.getAttribute("loginUser");
+		String[] statusArr = null;
+		if (StringUtils.isNotEmpty(status)){
+			statusArr = status.split(",");
+			if(Arrays.asList(statusArr).contains("-1"))
+				statusArr = new String[]{"34","90","0","62","64","60"};
+		}else
+			statusArr = new String[]{"34","90","0","62","64","60"};
+		return orderSvc.getAllOrder(pagination,loginUser,name,tel,infoerName,statusArr);
+	}
+	
+	@RequestMapping(value = "/getOrderById")
+	@ResponseBody
+	public Map<String, Object> getOrderById(@RequestParam("orderId") Integer orderId) throws Exception
+	{
+		Order order = orderSvc.getOrderById(orderId);
+		Map<String, Object> map = ResponseUtils.buildRespMapByBean(order);
+		return map;
+	}
+	
+	/**
+	 * 新增客户的回访记录
+	 * @param orderVisit
+	 * @param errors
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/addOrderVisit")
+	@ResponseBody
+	public Map<String, ?> addOrderVisit(@Valid OrderVisit orderVisit,Errors errors,HttpSession session) throws Exception
+	{
+		if(errors.hasErrors())
+			return ResponseUtils.buildRespMap(new BusinessException(ReturnCode.VALIDATE_FAIL.setMsg(errors.getFieldError().getDefaultMessage())));
+		User loginUser = (User) session.getAttribute("loginUser");
+		orderVisit.setVisitorId(loginUser.getId());
+		orderVisitSvc.addOrderVisit(orderVisit);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	/**
+	 * 申请在谈单
+	 * @param orderApprove
+	 * @param errors
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/applyOrder")
+	@ResponseBody
+	public Map<String, ?> applyOrder(@Valid OrderApprove orderApprove,Errors errors,HttpSession session) throws Exception
+	{
+		if(errors.hasErrors())
+			return ResponseUtils.buildRespMap(new BusinessException(ReturnCode.VALIDATE_FAIL.setMsg(errors.getFieldError().getDefaultMessage())));
+		User loginUser = (User) session.getAttribute("loginUser");
+		orderApprove.setClaimer(loginUser.getId());
+		orderApprove.setOperate(2);
+		orderSvc.addOrderApprove(orderApprove);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	@RequestMapping(value = "/permitOrder")
+	@ResponseBody
+	public Map<String, ?> permitOrder(@Valid OrderApprove orderApprove,Errors errors,HttpSession session) throws Exception
+	{
+		if(errors.hasErrors())
+			return ResponseUtils.buildRespMap(new BusinessException(ReturnCode.VALIDATE_FAIL.setMsg(errors.getFieldError().getDefaultMessage())));
+		User loginUser = (User) session.getAttribute("loginUser");
+		orderApprove.setOperate(1);
+		orderApprove.setApprover(loginUser.getId());
+		orderSvc.addOrderApprove(orderApprove);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	@RequestMapping(value = "/rejectOrder")
+	@ResponseBody
+	public Map<String, ?> rejectOrder(@Valid OrderApprove orderApprove,Errors errors,HttpSession session) throws Exception
+	{
+		if(errors.hasErrors())
+			return ResponseUtils.buildRespMap(new BusinessException(ReturnCode.VALIDATE_FAIL.setMsg(errors.getFieldError().getDefaultMessage())));
+		User loginUser = (User) session.getAttribute("loginUser");
+		if(!(loginUser.isMarketingManager() || loginUser.isDesignManager()) && !loginUser.isSuperAdmin())
+			return ResponseUtils.buildRespMap(ReturnCode.VALIDATE_FAIL.setMsg("对不起，您不是经理职务，无法执行此操作。"));
+		orderApprove.setOperate(0);
+		orderApprove.setApprover(loginUser.getId());
+		orderSvc.addOrderApprove(orderApprove);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	/**
+	 * 编辑订单
+	 * @param orderForm
+	 * @param errors
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/editOrder")
+	@ResponseBody
+	public Map<String, ?> editOrder(@Valid Order orderForm, Errors errors) throws Exception
+	{
+		if(errors.hasErrors())
+			return ResponseUtils.buildRespMap(new BusinessException(ReturnCode.VALIDATE_FAIL));
+		Client client = clientSvc.getClientByOrderId(orderForm.getId());
+		client.setName(orderForm.getName());
+		client.setOrgAddr(orderForm.getOrgAddr());
+		Order order = orderSvc.getOrderById(orderForm.getId());
+		order.setProjectName(orderForm.getProjectName());
+		order.setProjectAddr(orderForm.getProjectAddr());
+		orderSvc.updateOrder(order,client);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	/**
+	 * 获取业务员回访记录
+	 * @param orderId
+	 * @param pagination
+	 * @param session
+	 * @return
+	 * @throws BusinessException
+	 */
+	@RequestMapping(value = "/getOrderVisitByOrder")
+	@ResponseBody
+	public DatagridVo<OrderVisit> getOrderVisitByOrder(@RequestParam("orderId") Integer orderId, Pagination pagination,HttpSession session) throws BusinessException
+	{
+		User loginUser = (User) session.getAttribute("loginUser");
+		return orderVisitSvc.getAllOrderVisit(pagination, loginUser.getId(), orderId);
+	}
+	
+	/**
+	 * 获取设计师回访记录
+	 * @param orderId
+	 * @param pagination
+	 * @param session
+	 * @return
+	 * @throws BusinessException
+	 */
+	@RequestMapping(value = "/getStylistOrderVisitByOrder")
+	@ResponseBody
+	public DatagridVo<OrderVisit> getStylistOrderVisitByOrder(@RequestParam("orderId") Integer orderId, Pagination pagination,HttpSession session) throws BusinessException
+	{
+		Order order = orderSvc.getOrderById(orderId);
+		return orderVisitSvc.getAllOrderVisit(pagination, order.getDesignerId(), orderId);
+	}
+	
+	/**
+	 * 放弃客户
+	 * @param orderIds
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/deleteOrderByIds")
+	@ResponseBody
+	public Map<String, ?> deleteOrderByIds(@RequestParam("delIds[]") Integer[] orderIds, HttpSession session) throws Exception
+	{
+		if(ArrayUtils.isEmpty(orderIds))
+			return ResponseUtils.buildRespMap(ReturnCode.VALIDATE_FAIL.setMsg("没有可放弃的客户"));
+		orderSvc.deleteOrderByIds(orderIds);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	/**
+	 * 根据业务员查询信息员
+	 * @param session
+	 * @param pagination
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/findInfoerBySalesmanId")
+	@ResponseBody
+	public Map<String, ?> findInfoerBySalesmanId(HttpSession session,Pagination pagination) throws Exception
+	{
+		User loginUser = (User) session.getAttribute("loginUser");
+		if (loginUser == null)
+			return ResponseUtils.buildRespMap(ReturnCode.SESSION_TIME_OUT);
+		return ResponseUtils.buildRespMapByBean(infoerSvc.findBySalesmanId(loginUser.getId(),pagination));
+	}
+	
+	@RequestMapping(value = "/getInfoCostByOrder")
+	@ResponseBody
+	public DatagridVo<InfoCost> getInfoCostByOrder(@RequestParam("orderId") Integer orderId,Pagination pagination) throws BusinessException
+	{
+		return infoCostSvc.getAllInfoCost(pagination,null,orderId);
+	}
+	
+	@RequestMapping(value = "/getCommissionCostByOrder")
+	@ResponseBody
+	public DatagridVo<CommissionCost> getCommissionCostByOrder(@RequestParam("orderId") Integer orderId,Pagination pagination) throws BusinessException
+	{
+		return commissionCostSvc.getAllCommissionCost(pagination,null,orderId);
+	}
+	
+	@RequestMapping("/showSelectInfoerWindow")
+	public String showSelectInfoerWindow()
+	{
+		return "marketing/clientTrace/selectInfoer";
+	}
+	
+	@RequestMapping(value = "/getAllDesigner")
+	@ResponseBody
+	public DatagridVo<User> getAllDesigner(@RequestParam("userName") String userName,@RequestParam("alias") String alias,Pagination pagination) throws BusinessException
+	{
+		String[] roles = {"4","5","6"};//4：设计部设计师；5：设计部主管；6：设计部经理
+		return userSvc.getAllUserByRole(pagination, userName, alias, roles);
+	}
+	
+	@RequestMapping("/showAllSalesman")
+	public String showAllSalesman()
+	{
+		return "marketing/clientTrace/selectSalesman";
+	}
+	
+	@RequestMapping(value = "/getAllSalesman")
+	@ResponseBody
+	public DatagridVo<User> getAllSalesman(Pagination pagination) throws BusinessException
+	{
+		String[] roles = {"1","2","3"};//1：市场部业务员；2：市场部主管；3：市场部经理
+		return userSvc.getAllUserByRole(pagination, "", "", roles);
+	}
+	
+	@RequestMapping(value = "/transferOrder")
+	@ResponseBody
+	public Map<String, ?> transferOrder(@RequestParam("orderIds[]") Integer[] orderIds,@RequestParam("salesmanId") Integer salesmanId) throws Exception
+	{
+		orderSvc.updateOrderSalesmanId(orderIds,salesmanId);
+		return ResponseUtils.buildRespMap(ReturnCode.SUCCESS);
+	}
+	
+	@RequestMapping(value = "/showAddInfoCostWindow")
+	public ModelAndView showAddInfoCostWindow(HttpSession session, @RequestParam(value="orderId") Integer orderId)
+	{
+		ModelAndView mv = new ModelAndView("marketing/clientNegotiation/addInfoCost", "errorMsg", null);
+		User loginUser = (User) session.getAttribute("loginUser");
+		if(!loginUser.isMarketingManager() && !loginUser.isSuperAdmin())
+			mv.addObject("errorMsg", "对不起，你不是商务部经理，无法新增信息费。");
+		com.zoj.bp.costmgr.infocostmgr.vo.InfoCost infoCost = infoCostMgrSvc.getInfoCostByOrder(orderId);
+		if(infoCost.getCost() != null)		//已打款
+			mv.addObject("errorMsg", MessageFormat.format("客户[{0}]已打款，无法再次打款，请刷新后重试。", infoCost.getClientName()));
+		mv.addObject("infoCost", infoCost);
+		return mv;
+	}
+}
---
+++ src/main/webapp/WEB-INF/pages/design/index.jsp
@@ -0,0 +1,160 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title></title>
+</head>
+<body>
+<div class="easyui-layout" data-options="fit:true" style="margin: 2px;">
+	<div data-options="region:'center'" style="width: 470px;">
+		<div id="orderCheckMgrTab">
+			<div title="在谈单查询" border=false>
+				<table id="orderDatagrid" border=false></table>
+				<div id="orderDatagridToolbar">
+					<label style="vertical-align: middle;">名称：</label>
+					<input style="width:100px;" class="easyui-textbox" id="clientTrace.nameInput"/>
+					<label style="vertical-align: middle;">电话：</label>
+					<input style="width:100px;" class="easyui-textbox" id="clientTrace.telInput"/>
+					<label style="vertical-align: middle;">信息员名称：</label>
+					<input style="width:100px;" class="easyui-textbox" id="clientTrace.infoerNameInput"/>
+					<label style="vertical-align: middle;">状态筛选：</label>
+					<label><input type="checkbox" value="-1" name="statusInput" checked="checked"/>全部</label>
+					<label><input type="checkbox" value="34" name="statusInput"/>在谈单已批准</label>
+					<label><input type="checkbox" value="90" name="statusInput"/>已签单</label>
+					<label><input type="checkbox" value="0" name="statusInput"/>死单</label>
+					<label><input type="checkbox" value="60,62" name="statusInput"/>不准单审核中</label>
+					<label><input type="checkbox" value="64" name="statusInput"/>不准单</label>
+					<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-search" plain="true" id="queryOrderBtn">查询</a>
+					<!-- <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" id="showAddOrderWindowBtn">新增</a>
+					<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" id="removeOrderBtn">放弃</a>
+					<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" id="applyOrderBtn">申请在谈单</a> -->
+				</div>
+			</div>
+			<div title="审核" border=false>
+				<table id="orderCheckDatagrid" border=false></table>
+				<div id="orderCheckDatagridToolbar">
+					<label style="vertical-align: middle;">名称：</label>
+					<input style="width:100px;" class="easyui-textbox" id="order.nameInput"/>
+					<label style="vertical-align: middle;">电话：</label>
+					<input style="width:100px;" class="easyui-textbox" id="order.telInput"/>
+					<label style="vertical-align: middle;">信息员名称：</label>
+					<input style="width:100px;" class="easyui-textbox" id="order.infoerNameInput"/>
+					<label style="vertical-align: middle;">状态筛选：</label>
+					<label><input type="checkbox" value="30" name="orderStatusInput" />在谈单申请</label>
+					<label><input type="checkbox" value="62" name="orderStatusInput"/>不准单申请</label>
+					<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-search" plain="true" id="queryCheckOrderBtn">查询</a>
+					<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" id="showPermitOrderWindowBtn">批准</a>
+					<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" id="showRejectOrderWindowBtn">驳回</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div data-options="region:'south', split:true, border: true" style="height: 270px;">
+		<div id="clientMgrTab">
+			<div title="详情" border=false>
+				<form id="editOrderForm" action="marketing/clientMgr/editOrder" method="post" style="width: 100%;">
+					<input type="hidden" name="id">
+					<table style="width: 100%; min-width: 700px;">
+						<tr>
+							<td align="right" style="min-width: 80px;"><label>名称：</label></td>
+							<td style="min-width: 200px;"><input name="name" class="easyui-textbox" required="required"/></td>
+							<td align="right" style="min-width: 80px;"><label>联系电话：</label></td>
+							<td width="70%"><input name="telAll" readonly="readonly" class="easyui-textbox" required="required"/></td>
+						</tr>
+						<tr>
+							<td align="right" style="min-width: 80px;"><label>信息员：</label></td>
+							<td><input name="infoerName" readonly="readonly" class="easyui-textbox"/></td>
+							<td align="right" style="min-width: 80px;"><label>业务员：</label></td>
+							<td><input name="salesmanName" readonly="readonly" class="easyui-textbox"/></td>
+						</tr>
+						<tr>
+							<td align="right" style="min-width: 80px;"><label>设计师：</label></td>
+							<td><input name="designerName" readonly="readonly" class="easyui-textbox"/></td>
+							<td align="right" style="min-width: 80px;"><label>录入日期：</label></td>
+							<td><input name="insertTime" readonly="readonly" class="easyui-textbox"/></td>
+						</tr>
+						<tr>
+							<td align="right"><label>单位地址：</label></td>
+							<td colspan="3"><input name="orgAddr" style="width: 459px;" class="easyui-textbox" required="required"/></td>
+						</tr>
+						<tr>
+							<td align="right"><label>工程名称：</label></td>
+							<td colspan="3"><input name="projectName" style="width: 459px;" class="easyui-textbox" required="required"/></td>
+						</tr>
+						<tr>
+							<td align="right"><label>工程地址：</label></td>
+							<td colspan="3"><input name="projectAddr" style="width: 459px;" class="easyui-textbox" required="required"/></td>
+						</tr>
+						<tr>
+							<td align="center" colspan="3">
+								<a id="submitUpdateClientFormBtn" href="javascript:void(0)"  iconCls="icon-save" style="width: 60px;">保存</a>
+								<a id="refreshUpdateClientFormBtn" href="javascript:void(0)" iconCls="icon-reload" style="width: 60px;">刷新</a>
+							</td>
+						</tr>
+					</table>
+				</form>
+			</div>
+			<div title="业务员回访记录" border=false>
+				<table style="height: 100%; width: 100%;">
+					<tr>
+						<td>
+				    		<table id="orderVisitGrid"></table>
+				    		<div id="orderVisitGridToolbar">
+								<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" id="addOrderVisitBtn">新增</a>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div title="设计师回访记录" border=false>
+				<table style="height: 100%; width: 100%;">
+					<tr>
+						<td>
+				    		<table id="orderStylistVisitGrid"></table>
+						</td>
+					</tr>
+				</table>
+			</div>
+				<div title="信息费" border="false">
+				<table style="height: 100%; width: 100%;">
+					<tr>
+						<td>
+				    		<table id="infoCostGrid"></table>
+				    		<div id="infoCostGridToolbar">
+								<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" id="addInfoCostBtn">新增</a>
+								<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-reload" plain="true" id="reloadInfoCostBtn">刷新</a>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div title="提成" border="false">
+				<table style="height: 100%; width: 100%;">
+					<tr>
+						<td>
+				    		<table id="commissionCostGrid"></table>
+				    		<div id="commissionCostGridToolbar">
+								<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" id="addCommissionCostBtn">新增</a>
+								<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-reload" plain="true" id="reloadCommissionCostBtn">刷新</a>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="addClientVisitWindow"></div>
+<div id="addClientWindow"></div>
+<div id="permitOrderWindow"></div>
+<div id="rejectOrderWindow"></div>
+<div id="selectInfoerWindow"></div>
+<div id="showAddInfoCostWindow"></div>
+<script type="text/javascript" src="pages/marketing/clientNegotiation/index.js"></script>
+</body>
+</html>
