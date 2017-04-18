d9f282f01ad07184fcbb2bf7a6196b73d05d8bc2
Matchstick <Matchstick@Matchstick-001>
2017-04-18 22:14:30 +0800

    问题反馈-2014-04-08:2

--- src/main/webapp/WEB-INF/pages/marketing/clientTrace/index.js
+++ src/main/webapp/WEB-INF/pages/marketing/clientTrace/index.js
@@ -47,7 +47,7 @@ $(function()
 			  	{field:'isKey', hidden: true},
 				{field:'id', title:'单号', width: 3},
 				{field:'name', title:'名称', width: 3},
-				{field:'telAll', title:'联系电话'},
+				{field:'telAll', title:'联系电话', width: 5},
 				{field:'orgAddr', title:'单位地址', width: 8},
 				{field:'projectName', title:'工程名称', width: 8},
 				{field:'projectAddr', title:'面积'},
